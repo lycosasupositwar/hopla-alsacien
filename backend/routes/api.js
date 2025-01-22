1a1e582759beb15a74764c1dfc12a5e6bffe09a6
lycosasupositwar <garnier.gaetan68@gmail.com>
2025-01-23 00:26:45 +0100

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,17 +3,17 @@ const router = express.Router();
 
 // Données de test
 const alsacienData = [
-  { id: 1, word: "Guten Tach", translation: "Bonjour" },
-  { id: 2, word: "Wie geht's?", translation: "Comment ça va ?" },
-  { id: 3, word: "Mache's guet", translation: "Au revoir" }
+    { id: 1, word: "Guten Tach", translation: "Bonjour" },
+    { id: 2, word: "Wie geht's?", translation: "Comment ça va ?" },
+    { id: 3, word: "Mache's guet", translation: "Au revoir" }
 ];
 
 router.get('/mots', async (req, res) => {
-  const motsAvecAudio = await Promise.all(alsacienData.map(async mot => {
-    const audioUrl = await req.app.locals.generateAudio(mot.word);
-      return {...mot, audio: audioUrl};
-  }));
-  res.json(motsAvecAudio);
+    const motsAvecAudio = await Promise.all(alsacienData.map(async mot => {
+            const audioUrl = await req.app.locals.generateAudio(mot.word);
+            return {...mot, audio: audioUrl};
+        }));
+    res.json(motsAvecAudio);
 });
 
 module.exports = router;
