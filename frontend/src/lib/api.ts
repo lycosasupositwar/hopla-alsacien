7a26a6f99aa28749ac697ec87c87aa5b182d8ec2
lycosasupositwar <garnier.gaetan68@gmail.com>
2025-08-19 14:37:34 +0200

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -7,7 +7,7 @@ interface AnalysisParams {
 }
 
 // The base URL for our backend API
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'http://localhost:8050/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
